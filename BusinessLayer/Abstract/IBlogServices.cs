d9472e861a902f64111d9a962ad6a57e3ab2f23d
HUAWEI <HUAWEI@HUWAIE>
2023-04-05 17:24:00 +0300

    Add project files.

---
+++ BusinessLayer/Abstract/IBlogServices.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogServices
+    {
+        void Add(Blog blog);
+        void Update(Blog blog);
+        void Delete(Blog blog);
+        List<Blog> GetAll();
+        Blog BlogGetById(int id);
+    }
+
+}
---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,68 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        Context c = new Context(); 
+        public void AddBlog(Blog blog)
+        {
+           c.Add(blog);
+            c.SaveChanges();
+        }
+
+        public void Delete(Blog entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            c.Remove(blog);
+            c.SaveChanges();
+        }
+
+        public List<Blog> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetBlogId(int id)
+        {
+           return c.Blogs.Find(id);
+        }
+
+        public List<Blog> GetBlogs()
+        {
+            return c.Blogs.ToList();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Blog entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Blog entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
