44bc87420834d33d375e88e21dc061bd633cad14
sevca <sevca@SEVCAN>
2023-06-01 12:47:20 +0300

    .

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,74 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values=c.Employees.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new Context();
+            var values = c.Employees.Find(id);
+            if (values==null)
+            {
+                return BadRequest(string.Empty);
+            }
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult EmployeeAdd(Employee employee)
+        {
+            using var c = new Context();
+            if (employee == null)
+            {
+                return BadRequest(string.Empty);
+            }
+            c.Employees.Add(employee);
+            c.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new Context();
+            var value= c.Employees.Find(employee.Id);
+            if (employee == null)
+            {
+                return BadRequest(string.Empty);
+            }
+            value.Name=employee.Name;
+            c.Employees.Update(value);
+            c.SaveChanges();
+            return Ok(value);
+        }
+
+        [HttpDelete]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var value = c.Employees.Find(id);
+            if (value == null)
+            {
+                return BadRequest(string.Empty);
+            }
+
+            c.Employees.Remove(value);
+            c.SaveChanges();
+            return Ok(value);
+        }
+    }
+}
---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS03;database=CoreBlogApiDb;integrated security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Employee>Employees { get; set; }
+    }
+}
